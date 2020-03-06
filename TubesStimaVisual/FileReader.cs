1e4226ae203b44e0fc587f8ee454883fd350cb7a
margunwa123 <margunwa123@gmail.com>
2020-03-06 18:28:58 +0700

    Visual dah bisa didevelop

--- TubesStimaVisual/FileReader.cs
+++ TubesStimaVisual/FileReader.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace TubesStima2CoronaVirus
+namespace TubesStimaVisual
 {
     class PopulationReader : TxtReader
     {
