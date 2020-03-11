86f79c09724358f002b3800fa031acf9b9330579
margunwa123 <margunwa123@gmail.com>
2020-03-11 16:20:32 +0700

    hehe

--- TubesStimaVisual/src/Form1.Designer.cs
+++ TubesStimaVisual/src/Form1.Designer.cs
@@ -33,8 +33,9 @@
             this.howToTitle = new System.Windows.Forms.Label();
             this.howToStep = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.playButton = new System.Windows.Forms.Label();
+            this.exitHowToButton = new System.Windows.Forms.Label();
+            this.subtitle = new System.Windows.Forms.Label();
+            this.simulateButton = new System.Windows.Forms.Label();
             this.howToButton = new System.Windows.Forms.Label();
             this.exitButton = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
@@ -45,14 +46,13 @@
             this.title.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.title.AutoSize = true;
             this.title.BackColor = System.Drawing.Color.Transparent;
-            this.title.Font = new System.Drawing.Font("Gadugi", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Font = new System.Drawing.Font("Gadugi", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.title.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.title.Location = new System.Drawing.Point(94, 95);
-            this.title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.title.Location = new System.Drawing.Point(178, 100);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(412, 34);
+            this.title.Size = new System.Drawing.Size(423, 80);
             this.title.TabIndex = 0;
-            this.title.Text = "CoronaVirus Spread Simulator";
+            this.title.Text = "CoronaVirus";
             this.title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // howToTitle
@@ -62,63 +62,84 @@
             this.howToTitle.BackColor = System.Drawing.Color.Black;
             this.howToTitle.Font = new System.Drawing.Font("Gadugi", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.howToTitle.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.howToTitle.Location = new System.Drawing.Point(210, 75);
+            this.howToTitle.Location = new System.Drawing.Point(239, 92);
+            this.howToTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.howToTitle.Name = "howToTitle";
-            this.howToTitle.Size = new System.Drawing.Size(179, 34);
+            this.howToTitle.Size = new System.Drawing.Size(301, 41);
             this.howToTitle.TabIndex = 4;
-            this.howToTitle.Text = "How To Play";
+            this.howToTitle.Text = "How To Simulate";
             // 
             // howToStep
             // 
             this.howToStep.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.howToStep.AutoSize = true;
             this.howToStep.BackColor = System.Drawing.Color.Black;
-            this.howToStep.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.howToStep.Font = new System.Drawing.Font("Gadugi", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.howToStep.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.howToStep.Location = new System.Drawing.Point(275, 144);
+            this.howToStep.Location = new System.Drawing.Point(170, 150);
+            this.howToStep.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.howToStep.Name = "howToStep";
-            this.howToStep.Size = new System.Drawing.Size(51, 50);
+            this.howToStep.Size = new System.Drawing.Size(440, 196);
             this.howToStep.TabIndex = 5;
-            this.howToStep.Text = "1. A\n2. B";
+            this.howToStep.Text = "1. Click the Simulation Button\n\n2. Choose your population and graph\n    text file" +
+    "s location\n\n3. Select the number of days the\n    spreading occurs";
             // 
             // groupBox1
             // 
             this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.groupBox1.BackColor = System.Drawing.Color.Black;
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(100, 50);
+            this.groupBox1.Controls.Add(this.exitHowToButton);
+            this.groupBox1.Location = new System.Drawing.Point(123, 62);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(400, 250);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(533, 308);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
-            // label1
+            // exitHowToButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label1.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(372, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 21);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "X";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.exitHowToButton.AutoSize = true;
+            this.exitHowToButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitHowToButton.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitHowToButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.exitHowToButton.Location = new System.Drawing.Point(500, 12);
+            this.exitHowToButton.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.exitHowToButton.Name = "exitHowToButton";
+            this.exitHowToButton.Size = new System.Drawing.Size(28, 28);
+            this.exitHowToButton.TabIndex = 0;
+            this.exitHowToButton.Text = "X";
+            this.exitHowToButton.Click += new System.EventHandler(this.exitHowToButton_Click);
             // 
-            // playButton
+            // subtitle
             // 
-            this.playButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.playButton.AutoSize = true;
-            this.playButton.BackColor = System.Drawing.Color.Transparent;
-            this.playButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.playButton.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.playButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.playButton.Location = new System.Drawing.Point(279, 170);
-            this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(43, 19);
-            this.playButton.TabIndex = 1;
-            this.playButton.Text = "Play!";
-            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            this.subtitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.subtitle.AutoSize = true;
+            this.subtitle.BackColor = System.Drawing.Color.Transparent;
+            this.subtitle.Font = new System.Drawing.Font("Berlin Sans FB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitle.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.subtitle.Location = new System.Drawing.Point(229, 168);
+            this.subtitle.Name = "subtitle";
+            this.subtitle.Size = new System.Drawing.Size(322, 30);
+            this.subtitle.TabIndex = 0;
+            this.subtitle.Text = "S   I   M   U   L   A   T   O   R";
+            this.subtitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // simulateButton
+            // 
+            this.simulateButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.simulateButton.AutoSize = true;
+            this.simulateButton.BackColor = System.Drawing.Color.Transparent;
+            this.simulateButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.simulateButton.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simulateButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.simulateButton.Location = new System.Drawing.Point(337, 227);
+            this.simulateButton.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.simulateButton.Name = "simulateButton";
+            this.simulateButton.Size = new System.Drawing.Size(106, 21);
+            this.simulateButton.TabIndex = 1;
+            this.simulateButton.Text = "Simulation";
+            this.simulateButton.Click += new System.EventHandler(this.simulateButton_Click);
             // 
             // howToButton
             // 
@@ -128,11 +149,12 @@
             this.howToButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.howToButton.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.howToButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.howToButton.Location = new System.Drawing.Point(252, 200);
+            this.howToButton.Location = new System.Drawing.Point(364, 264);
+            this.howToButton.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.howToButton.Name = "howToButton";
-            this.howToButton.Size = new System.Drawing.Size(96, 19);
+            this.howToButton.Size = new System.Drawing.Size(52, 21);
             this.howToButton.TabIndex = 7;
-            this.howToButton.Text = "How To Play";
+            this.howToButton.Text = "Help";
             this.howToButton.Click += new System.EventHandler(this.howToButton_Click);
             // 
             // exitButton
@@ -143,30 +165,32 @@
             this.exitButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.exitButton.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exitButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.exitButton.Location = new System.Drawing.Point(283, 230);
+            this.exitButton.Location = new System.Drawing.Point(368, 301);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(35, 19);
+            this.exitButton.Size = new System.Drawing.Size(43, 21);
             this.exitButton.TabIndex = 8;
             this.exitButton.Text = "Exit";
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ClientSize = new System.Drawing.Size(779, 444);
+            this.Controls.Add(this.subtitle);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.howToButton);
-            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.simulateButton);
             this.Controls.Add(this.howToStep);
             this.Controls.Add(this.howToTitle);
             this.Controls.Add(this.title);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -184,10 +208,11 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label howToTitle;
         private System.Windows.Forms.Label howToStep;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label playButton;
+        private System.Windows.Forms.Label exitHowToButton;
+        private System.Windows.Forms.Label simulateButton;
         private System.Windows.Forms.Label howToButton;
         private System.Windows.Forms.Label exitButton;
+        private System.Windows.Forms.Label subtitle;
     }
 }
 
--- TubesStimaVisual/src/ShowGraph.Designer.cs
+++ TubesStimaVisual/src/ShowGraph.Designer.cs
@@ -28,60 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.graphOpen = new System.Windows.Forms.Button();
+            this.populationLabel = new System.Windows.Forms.Label();
+            this.graphLabel = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.populationTextBox = new System.Windows.Forms.TextBox();
+            this.graphTextBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.simulate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // graphOpen
             // 
-            this.button1.Location = new System.Drawing.Point(267, 67);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.graphOpen.Location = new System.Drawing.Point(381, 157);
+            this.graphOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.graphOpen.Name = "graphOpen";
+            this.graphOpen.Size = new System.Drawing.Size(100, 28);
+            this.graphOpen.TabIndex = 0;
+            this.graphOpen.Text = "Choose";
+            this.graphOpen.UseVisualStyleBackColor = true;
+            this.graphOpen.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // populationLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(290, 115);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.populationLabel.AutoSize = true;
+            this.populationLabel.Location = new System.Drawing.Point(387, 142);
+            this.populationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.populationLabel.Name = "populationLabel";
+            this.populationLabel.Size = new System.Drawing.Size(0, 17);
+            this.populationLabel.TabIndex = 1;
             // 
-            // label2
+            // graphLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(290, 149);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.graphLabel.AutoSize = true;
+            this.graphLabel.Location = new System.Drawing.Point(387, 183);
+            this.graphLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.graphLabel.Name = "graphLabel";
+            this.graphLabel.Size = new System.Drawing.Size(0, 17);
+            this.graphLabel.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(267, 267);
+            this.button2.Location = new System.Drawing.Point(356, 329);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 3;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // populationTextBox
+            // 
+            this.populationTextBox.Location = new System.Drawing.Point(259, 203);
+            this.populationTextBox.Name = "populationTextBox";
+            this.populationTextBox.Size = new System.Drawing.Size(100, 22);
+            this.populationTextBox.TabIndex = 4;
+            // 
+            // graphTextBox
+            // 
+            this.graphTextBox.Location = new System.Drawing.Point(259, 163);
+            this.graphTextBox.Name = "graphTextBox";
+            this.graphTextBox.Size = new System.Drawing.Size(100, 22);
+            this.graphTextBox.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Choose";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // simulate
+            // 
+            this.simulate.Location = new System.Drawing.Point(562, 162);
+            this.simulate.Name = "simulate";
+            this.simulate.Size = new System.Drawing.Size(75, 23);
+            this.simulate.TabIndex = 7;
+            this.simulate.Text = "button4";
+            this.simulate.UseVisualStyleBackColor = true;
+            this.simulate.Click += new System.EventHandler(this.simulate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Select a Graph File";
+            // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(779, 444);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.simulate);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.graphTextBox);
+            this.Controls.Add(this.populationTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.graphLabel);
+            this.Controls.Add(this.populationLabel);
+            this.Controls.Add(this.graphOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
@@ -93,9 +149,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button graphOpen;
+        private System.Windows.Forms.Label populationLabel;
+        private System.Windows.Forms.Label graphLabel;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox populationTextBox;
+        private System.Windows.Forms.TextBox graphTextBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openPopulation;
+        private System.Windows.Forms.Button simulate;
+        private System.Windows.Forms.Label label1;
     }
 }
